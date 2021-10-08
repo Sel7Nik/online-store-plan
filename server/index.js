b1f12636984d2444bec98c230cf0dcb4d2a5ef55
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-09 02:09:25 +0300

    require('dotenv').config(); onst PORT = process.env.PORT

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+// require('dotenv').config();
+require('dotenv').config();
+
 const express = require('express');
 // import express from 'express';
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
