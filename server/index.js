fa5f222a5710001d69b6214c7af2fbe914a5e762
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-09 01:56:35 +0300

    app.listen app.listen(PORT, () => console.log('START SERVER ON PORT ${PORT}'));

--- server/index.js
+++ server/index.js
@@ -1,2 +1,7 @@
 const express = require('express');
 // import express from 'express';
+const PORT = 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log('START SERVER ON PORT ${PORT}'));
