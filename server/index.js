0a491a8e1173e841739d9a170d27934d7f33fa1d
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-09 02:00:27 +0300

    start server npm run dev

--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ const PORT = 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log('START SERVER ON PORT ${PORT}'));
+app.listen(PORT, () => console.log(`START SERVER ON PORT ${PORT}`));
