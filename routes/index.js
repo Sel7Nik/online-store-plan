d2bfd233303429d80fe17d2257511db62eda0412
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-09 17:57:14 +0300

    import routes in routes/index.js const deviceRouter = require('./deviceRouter') const userRouter = require('./userRouter') const brandRouter = require('./brandRouter') const typeRouter = require('./typeRouter')

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,7 @@
 const Router = require('express');
 const router = new Router();
+
+const deviceRouter = require('./deviceRouter');
+const userRouter = require('./userRouter');
+const brandRouter = require('./brandRouter');
+const typeRouter = require('./typeRouter');
