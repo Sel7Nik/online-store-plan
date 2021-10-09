aec49ef0946eb8609ab66bd5ac22018c54b8f917
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-09 16:19:56 +0300

    START SERVER  error correction

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
-
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -13,9 +14,7 @@ const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    await sequelize.app.listen(PORT, () =>
-      console.log(`START SERVER ON PORT ${PORT}`)
-    );
+    app.listen(PORT, () => console.log(`START SERVER ON PORT ${PORT}`));
   } catch (err) {
     console.log(err);
   }
