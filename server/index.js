bae7f3b273f1e8c1cb1b8ab489324af76ff9c413
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-09 16:07:02 +0300

    app.use(cors()); app.use(express.json());

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const models = require('./models/models');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const start = async () => {
   try {
