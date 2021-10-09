24b4fcf3d604b75fd2237e8aae3f04b0dac30860
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-09 16:24:42 +0300

    app.get('/', (req, res) => {   res.status(200).json({ message: 'WORKING!!!' }); });

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'WORKING!!!' });
+});
+
 const start = async () => {
   try {
     await sequelize.authenticate();
