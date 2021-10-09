f11461a0fd3898fa049c5bb6441921780deca873
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-09 18:41:14 +0300

    error correction  28:15

--- routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express');
-const router = new Router();
-
-router.post('/registration', userController.registration);
-router.post('/login', userController.login);
-router.get('/auth', authMiddleware, userController.check);
-
-module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
 const Router = require('express');
 const router = new Router();
 
-const deviceRouter = require('./deviceRouter');
 const userRouter = require('./userRouter');
-const brandRouter = require('./brandRouter');
 const typeRouter = require('./typeRouter');
+const brandRouter = require('./brandRouter');
+const deviceRouter = require('./deviceRouter');
 
 router.use('/user', userRouter);
 router.use('/type', typeRouter);
